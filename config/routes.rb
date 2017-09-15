936461456ebd0055b0dff4ede09287a7c4d9e2f7
jkxruby <jkxruby@gmail.com>
2017-09-15 11:07:36 +0800

    add routes controller about trains

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
  namespace :api, :defaults => { :format => :json } do
    namespace :v1 do
-     get "/trains" => "train#index", :as => :trains
+     get "/trains" => "trains#index", :as => :trains
      get "/trains/:train_number" => "trains#show", :as => :train
 
      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
