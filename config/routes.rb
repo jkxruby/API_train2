53a2f15d8343991340ce319940cc166bc5f98121
jkxruby <jkxruby@gmail.com>
2017-09-15 16:20:30 +0800

    install devise and modify the model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
  namespace :api, :defaults => { :format => :json } do
    namespace :v1 do
      get "/trains" => "trains#index", :as => :trains
@@ -14,5 +15,7 @@ Rails.application.routes.draw do
      delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
    end
  end
+
+root "welcome#index"
 
 end
